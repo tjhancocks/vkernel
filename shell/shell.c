39e4b50edc4fa5d5452ecb712c9cd7da5961cc17
Tom Hancocks <tomhancocks@gmail.com>
2019-01-24 19:19:55 +0000

    Continue fleshing out the basic kernel shell.
    This continues to add in more functionality to the built in kernel
    shell. This will allow it to be useful for testing and debugging
    purposes.
    
    It is now capable of loading kernel shell scripts from the ramdisk.

--- shell/shell.c
+++ shell/shell.c
@@ -24,23 +24,145 @@
 #include <thread.h>
 #include <print.h>
 #include <string.h>
+#include <alloc.h>
+#include <ramdisk.h>
 
 ////////////////////////////////////////////////////////////////////////////////
+
+#define BUFFER_LEN	1024
 
 static struct thread *kernel_shell_thread = NULL;
 
+static void ksh_parse_command(const char *restrict buffer, bool *exit);
+static void ksh_handle_command(uint8_t argc, const char **argv, bool *exit);
+static void ksh_run_script(const char *restrict script, bool *exit);
+
 ////////////////////////////////////////////////////////////////////////////////
+
+static inline bool isspace(char c)
+{
+	return (c == ' ' || c == '\t');
+}
+
+static void ksh_run_script(const char *restrict script, bool *exit)
+{
+	char statement[BUFFER_LEN] = { 0 };
+	uint32_t ptr = 0;
+
+	while (*script) {
+		if (*script == '\n') {
+			statement[ptr] = '\0';
+			ksh_parse_command(statement, exit);
+			memset(statement, '\0', BUFFER_LEN);
+			ptr = 0;
+		} 
+		else if (ptr < BUFFER_LEN) {
+			statement[ptr++] = *script;
+		}
+		++script;
+	}
+}
+
+static void ksh_handle_command(uint8_t argc, const char **argv, bool *exit)
+{
+	if (strcmp(argv[0], "exit") == 0) {
+		*exit = true;
+	}
+	else if (strcmp(argv[0], "syscall") == 0) {
+		/* system call - requires at least 1 argument (argc >= 2) */
+		if (argc >= 2) {
+			/* todo */
+		}
+		else {
+			kprint("Insufficient arguments provided.\n");
+			kprint("  syscall [function_number] [...]\n");
+		}
+	}
+	else if (strcmp(argv[0], "echo") == 0) {
+		for (uint8_t i = 1; i < argc; ++i) {
+			kprint("%s\n", argv[i]);
+		}
+	}
+	else {
+		char *script = ramdisk_open(&system_ramdisk, argv[0], NULL);
+		if (script) {
+			ksh_run_script(script, exit);
+			kfree(script);
+		}
+		else {
+			kprint("Unrecognised command '%s'\n", argv[0]);
+		}
+	}
+}
 
 static void ksh_parse_command(const char *restrict buffer, bool *exit)
 {
-	if (strcmp(buffer, "exit") == 0) {
-		*exit = true;
+	/* parse the command into its components. we can then see if there are
+	   any commands built into the ramdisk for use. */
+	char *ptr = buffer;
+	char *tail = ptr + strlen(ptr) - 1;
+	while (isspace(*ptr)) ++ptr;
+	while (isspace(*tail)) *tail-- = '\0';
+
+	/* Is the command a comment, or an empty line */
+	if (*ptr == '#' || strlen(buffer) == 0) return;
+
+	/* set up storage for the arguments - 16 arguments maximum */
+	char *argv[16] = { NULL };
+	uint8_t argc = 0;
+
+	uint32_t ptr_len = strlen(ptr);
+	uint32_t start = 0;
+	uint32_t length = 0;
+	bool escaped = false;
+	bool in_string = false;
+
+	for (uint32_t i = 0; i < ptr_len + 1; ++i) {
+		char c = ptr[i];
+
+		if ((!escaped && !in_string && isspace(c)) || c == '\0') {
+			if ((length = i - start) > 0) {
+				if (length >= 2 && ptr[start] == '"') {
+					++start;
+				}
+				if (length >= 2 && ptr[start + length - 2] == '"') {
+					length -= 2;
+				}
+
+				char *token = memcpy(kalloc(length + 1), ptr + start, length);
+				token[length] = '\0';
+				argv[argc++] = token;
+			}
+
+			start = i + 1;
+		}
+		else if (!escaped && c == '"' && !in_string) {
+			in_string = true;
+			start = i;
+		}
+		else if (!escaped && c == '"' && in_string) {
+			in_string = false;
+		}
+		else if (c == '\\') {
+			escaped = true;
+			continue;
+		}
+
+		escaped = false;
+
+		/* we reached the maximum arguments? */
+		if (argc >= 16) {
+			kprint("Too many arguments!\n");
+			break;
+		}
 	}
-	else if (strcmp(buffer, "uname") == 0) {
-		kprint("vkernel\n");
-	}
-	else {
-		kprint("Unrecognised command '%s'\n", buffer);
+
+	/* attempt to handle the command */
+	ksh_handle_command(argc, argv, exit);
+
+	/* clean up */
+	for (uint8_t i = 0; i < argc; ++i) {
+		kfree(argv[i]);
 	}
 }
 
@@ -50,11 +172,11 @@ int kernel_shell_main(void)
 {
 	bool should_exit = false;
 	while (!should_exit) {
-		char buffer[1024] = { 0 };
+		char buffer[BUFFER_LEN] = { 0 };
 		kprint("# ");
 
 		/* read input from the user and handle it */
-		if (readline(&buffer, 1024) > 0) {
+		if (readline(&buffer, BUFFER_LEN) > 0) {
 			kprint("\n");
 			ksh_parse_command(buffer, &should_exit);
 		}
